1023e0ef4d7e684d9c7efba83115bda53596a7a6
DESKTOP-2N249SV\4kigs_09 <luputona5>
2017-11-03 15:24:14 +0900

    no message

---
+++ Chapter_01/StringIO.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+void main4()
+{
+	char name[100];
+	char lang[100];
+
+	cout << "이름은 무엇 입니까? ";
+	cin >> name;
+
+	cout << " 좋아하는 프로그래밍 언어는 무엇 인가요??";
+	cin >> lang;
+
+	cout << "내 이름은 " << name << "입니다.\n";
+	cout << "제일 좋아하는 언어는 " << lang << "입니다." << endl;
+
+}
